80cbe90fa89fd70681a223305525e499e73baff8
Ani <ani@anionline.me>
2019-04-25 12:52:40 -0700

    Add minimal working sample that is correctly installed and generates a .cs file in the project directory.

---
+++ CustomTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CustomTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Arguments: {string.Join(' ', args)}");
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(args[0]), "CustomTool.g.cs"), "public sealed class Test {  }");
+        }
+    }
+}
---
+++ CustomToolTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CustomToolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allTypes = from type in Assembly.GetExecutingAssembly().GetTypes()
+                           select type.FullName;
+            foreach (var type in allTypes)
+                Console.WriteLine(type);
+        }
+    }
+}
